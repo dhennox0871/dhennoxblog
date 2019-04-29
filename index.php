7a8dde24d75f37d304fe14081478b8ad97457c62
dhenny hariyanto <49649443+dhennox0871@users.noreply.github.com>
2019-04-29 17:18:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,28 @@
 			  </div>
 		  </div>
 	  </div>
-	  
+	  <div class="container">
+		  <div class="row">
+		  	<div class="col-md-12">
+				<table class="table table-bordered">
+					<thead>
+						<th>Kode</th>
+						<th>Nama</th>
+						<th>Alamat</th>
+						<th>Kota</th>
+						<th>Email</th>
+					</thead>
+					<tbody>
+						<th>Kode</th>
+						<th>Nama</th>
+						<th>Alamat</th>
+						<th>Kota</th>
+						<th>Email</th>
+					</tbody>
+				</table>
+			  </div>
+		  </div>
+	  </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
