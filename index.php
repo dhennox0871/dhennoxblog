3d8886934e1ab0a5366bfbdf8516ef72c9fe4761
dhenny hariyanto <49649443+dhennox0871@users.noreply.github.com>
2019-05-01 13:43:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
 						<td><?php echo $row['Name'];?></td>
 						<td><?php echo $row['Address1'];?></td>
 						<td><?php echo $row['City'];?></td>
-						<td><?php echo $row['Entrydate'];?></td>
+						<td><?php echo $row['Entrydate'];print_r($row);?></td>
 						</tr>
 						<?php
 						}
