8d919a034f32ca0c3440a0bd1091e559970dd3f7
dhenny hariyanto <49649443+dhennox0871@users.noreply.github.com>
2019-05-01 13:28:10 +0700

    Update simpan.php

--- simpan.php
+++ simpan.php
@@ -5,7 +5,7 @@ $nama=$_POST['nama'];
 $alamat=$_POST['alamat'];
 $kota=$_POST['kota'];
 $email=$_POST['email'];
-$tgl=date('Y-M-d',now());
+$tgl=date('d-M-Y');
 $sqdp="insert into mastercust values('$kode','$nama','$alamat','$kota','$email','$tgl')";
 $save=$conn->prepare($sqdp);
 $save->execute();
